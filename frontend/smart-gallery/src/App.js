bd59a9136f647ca2dcd8f10b2864fcdb2596b413
Bdone-hub <67092457+Bdone-hub@users.noreply.github.com>
2023-11-16 16:32:48 +0100

    ajout switch routes

--- frontend/smart-gallery/src/App.js
+++ frontend/smart-gallery/src/App.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import GalleryImages from './GalleryImages';
+import Custom from './Custom'; // Remplacez CustomComponent par le nom de votre composant personnalisé
 
 function App() {
   return (
-    <div className="App">
-      <h1>Smart Gallery</h1>
+    <Router>
+      <div className="App">
+        <h1>Smart Gallery</h1>
 
-      <GalleryImages />
-    </div>
+        <Route path="/" exact component={GalleryImages} />
+        <Route path="/custom" component={Custom} />
+        {/* Ajoutez d'autres routes personnalisées ici si nécessaire */}
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/smart-gallery/src/Custom.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Custom() {
+  return (
+    <div>
+      <h2>Page Custom</h2>
+      <p>Cette page est accessible via /custom</p>
+    </div>
+  );
+}
+
+export default Custom;
